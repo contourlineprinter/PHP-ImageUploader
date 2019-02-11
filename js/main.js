b46f3d748199e6c5e91a72e7d19ea6aad89bb47f
kenny <kennysexton4@gmail.com>
2019-02-11 15:14:27 -0500

    all visual changes

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 		document.getElementById("filename").innerHTML = selectedFile;
 		document.getElementById("action-text").innerHTML = "Submit";
 		document.getElementById("main-icon").src="icons/arrow_white.svg";
-		document.getElementById('card').style.cssText = 'background: linear-gradient(to right, #FF9F4F, #FFAE6B);  color: white;';
+		document.getElementById('card').style.cssText = 'background: linear-gradient(to right, #F2E25C, #F5E451);  color: white;';
 		document.getElementById('main-icon').style.cssText = 'width: 20%;';	
 	};
 });
