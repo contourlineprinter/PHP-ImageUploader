b4a93439bf9a1f06313b7b53ed95e586f842ad4f
kenny <kennysexton4@gmail.com>
2019-03-13 11:46:57 -0400

    reupload with all recent changes that are live on the CIS-linux server

---
+++ php/uploadFile.php
@@ -0,0 +1,47 @@
+<?php
+	session_start();
+	ob_start();
+	$_SESSION["dataArray"] = NULL;
+	$target_dir = "../uploads/";
+	$temp = explode(".", $_FILES["file"]["name"]);
+	$file_name = "file1". '.' . strtolower(end($temp));
+	
+
+	print "cp1 <br>";
+
+	echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+	echo "Type: " . $_FILES["file"]["type"] . "<br>";
+	echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+	echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if (move_uploaded_file($_FILES["file"]["tmp_name"], "../uploads/".$file_name)){
+			print "cp3 <br>";
+		}
+		else {
+			print "Error: ".UPLOAD_ERR_OK;	
+		}
+	}
+
+	echo '<pre>';
+	
+	print "<br>";
+	chdir('../ExifRead-2.1.2/');
+	print "cp4 <br>";
+	exec('pwd', $out1);
+	var_dump($out1);
+	print "<br>";
+	chmod($file_name, 777);
+	exec('./EXIF.py ../uploads/'.$file_name.' 2>&1', $out);
+	$_SESSION["dataArray"] = $out;
+	var_dump($out);
+	echo '</pre>';
+	
+	
+	# Redirect to the results page
+	header ('location: ../results.html');
+	ob_end_flush();
+	
+	print "fin";
+	exit();
+?>
